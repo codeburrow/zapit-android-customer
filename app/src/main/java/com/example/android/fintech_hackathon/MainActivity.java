60458330c8917995e50002a4815aa338e1faf72a
grg <spiridakis.grg@gmail.com>
2016-04-15 18:45:16 +0300

    Create a ListView in MainActivity

--- app/src/main/java/com/example/android/fintech_hackathon/MainActivity.java
+++ app/src/main/java/com/example/android/fintech_hackathon/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.android.fintech_hackathon;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // List View
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get ListView from xml
+        mListView = (ListView) findViewById(R.id.list_view);
+
+
     }
 
 }
