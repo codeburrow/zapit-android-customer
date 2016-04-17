a3d7f59dadd4fb4e4815f6477d6bc5e47871f50c
george_spiridakis <spiridakis.grg@gmail.com>
2016-04-17 23:37:09 +0300

    Create ScanActivity

---
+++ app/src/main/java/com/example/android/fintech_hackathon/ScanActivity.java
@@ -0,0 +1,45 @@
+package com.example.android.fintech_hackathon;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ScanActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // LOG_TAG
+    private static final String TAG = "ScanActivity";
+    // Scan Button
+    private Button scanBtn;
+    // Text Views to display the format and the content of the barcode
+    private TextView formatTxt, contentTxt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+
+        /*
+            Get button and textViews from the xml
+         */
+        scanBtn = (Button)findViewById(R.id.scan_button);
+        formatTxt = (TextView)findViewById(R.id.scan_format);
+        contentTxt = (TextView)findViewById(R.id.scan_content);
+
+        scanBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        // Check whether the scanning button has been pressed.
+        if( v.getId() == R.id.scan_button ){
+            Log.e(TAG , "Scan button is pressed");
+
+
+        }
+
+    }
+}
