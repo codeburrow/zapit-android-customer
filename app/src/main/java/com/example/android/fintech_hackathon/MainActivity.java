1b01d5c1837057badf1e32c88304e0e681fc057a
george_spiridakis <spiridakis.grg@gmail.com>
2016-04-17 17:36:20 +0300

    Create an ArrayAdapter with dummy data

--- app/src/main/java/com/example/android/fintech_hackathon/MainActivity.java
+++ app/src/main/java/com/example/android/fintech_hackathon/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.android.fintech_hackathon;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     // List View
     private ListView mListView;
+    // ArrayAdapter
+    private ArrayAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +24,39 @@ public class MainActivity extends AppCompatActivity {
         mListView = (ListView) findViewById(R.id.list_view);
 
 
+        /**
+         *  Create an ArrayAdapter<String>
+         *
+         *  1. context
+         *  2. layout for the list item
+         *  3. id of the text view
+         *  4. array list - data to be used
+         */
+        mAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                createDummyDataArray(15));
+
+        // Set mAdapter to mListView
+        mListView.setAdapter(mAdapter);
     }
+
+    /**
+     *
+     * @param numberOfRows to be generated
+     * @return ArrayList<String> of dummy data
+     */
+    private ArrayList<String> createDummyDataArray(int numberOfRows) {
+        ArrayList<String> dummyDataArray = new ArrayList<>();
+
+        for (int i = 0; i <= numberOfRows; i ++){
+            dummyDataArray.add("CodeBurrow " + i);
+            Log.e("CodeBurrow", "" + i);
+        }
+
+        return dummyDataArray;
+    }
+
+
 
 }
