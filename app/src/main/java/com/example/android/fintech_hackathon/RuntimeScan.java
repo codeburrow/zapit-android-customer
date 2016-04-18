81dfd71789b00412e3b772760424f839ea86ca59
george_spiridakis <spiridakis.grg@gmail.com>
2016-04-18 11:37:16 +0300

    Create RuntimeScan Activity .

---
+++ app/src/main/java/com/example/android/fintech_hackathon/RuntimeScan.java
@@ -0,0 +1,13 @@
+package com.example.android.fintech_hackathon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RuntimeScan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_runtime_scan);
+    }
+}
